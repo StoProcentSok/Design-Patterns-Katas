99e334aa3c0999ba05dab9c797af6717148c6590
Michal <michalbuczek00@gmail.com>
2023-11-30 12:52:53 +0100

    Tests for singleton prep

---
+++ Creational/Singleton/Singleton_Tests/WhenSingletonIsCalled.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Singleton;
+
+namespace Singleton_Tests
+{
+    [TestFixture]
+    public class WhenSingletonIsCalled
+    {
+
+        [Test]
+        public void 
+
+        [Test]
+        public void ThenInitializationLogEntryIsAdded() 
+        {
+
+            //Logger
+        }
+
+    }
+
+
+    [TestFixture]
+    public class WhenSingletonIsNotCalled
+    {
+        [Test]
+        public void ThenNoEntryAboutInitializationIsPresent() 
+        {
+            var logs = Logger.GetLog();
+            Assert.
+        }
+    }
+}
