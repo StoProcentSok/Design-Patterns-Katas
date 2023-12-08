ccd587f9a28b34cef48ed99d250f11e3d11d8abb
Michal <michalbuczek00@gmail.com>
2023-12-08 15:29:00 +0100

    Cleanup

--- Structural/Facade/Program.cs
+++ Structural/Facade/Program.cs
@@ -8,16 +8,6 @@ public class Program
     {
         var bigCassFacade = new BigClassFacade();
         var result = bigCassFacade.DoOperations();
-        BigClass bigClass = new BigClass();
-
-        bigClass.SetValueI(3);
-
-        bigClass.IncrementI();
-        bigClass.IncrementI();
-        bigClass.IncrementI();
-
-        bigClass.DecrememntI();
-
 
         Console.WriteLine($"Final Number : {result}");
     }
