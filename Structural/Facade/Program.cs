578984ab4abcf422a86e9f2fea251a8cd91bba1d
Michal <michalbuczek00@gmail.com>
2023-12-08 14:48:20 +0100

    Facade project added

---
+++ Structural/Facade/Program.cs
@@ -0,0 +1,20 @@
+﻿using Facade;
+using System.Formats.Asn1;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        BigClass bigClass = new BigClass();
+
+        bigClass.SetValueI(3);
+
+        bigClass.IncrementI();
+        bigClass.IncrementI();
+        bigClass.IncrementI();
+
+        bigClass.DecrememntI();
+
+        Console.WriteLine($"Final Number : {bigClass.GetValueB()}");
+    }
+}
