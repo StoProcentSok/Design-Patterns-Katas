269fca1bf05693751c860229ae74d0b9793ec97f
Michal <michalbuczek00@gmail.com>
2024-01-17 18:13:17 +0100

    Initial project for Prototype pattern. Shallow copy with memberwiseClone and deep copy with json.

---
+++ Creational/Prototype/Implementation.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+
+namespace Prototype
+{
+    public abstract class Person
+    {
+        public abstract string Name { get; set; }
+
+        public abstract Person Clone(bool deepClone);
+    }
+
+    public class Manager : Person
+    {
+        public override string Name {get; set; }
+
+        public Manager(string name)
+        {
+            this.Name = name;
+        }
+
+        public override Person Clone(bool deepClone = false)
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+
+    public class Employee : Person
+    {
+        public override string Name { get; set; }
+        public Manager Manager { get; set; }
+
+        public Employee(string name, Manager manager)
+        {
+            this.Name = name;
+            this.Manager = manager;
+        }
+
+        public override Person Clone(bool deepClone = false)
+        {
+            if (deepClone)
+            {
+                var personSerialized = JsonSerializer.Serialize(this);
+                return JsonSerializer.Deserialize<Employee>(personSerialized);
+            }
+
+            return (Person)MemberwiseClone();
+        }
+    }
+}
---
+++ Creational/Prototype/Program.cs
@@ -0,0 +1,20 @@
+﻿using Prototype;
+
+Console.Title = "Prototype";
+
+var manager = new Manager("Manager Karen");
+
+var employee = new Employee("Employee Rob", manager);
+
+Console.WriteLine($"employee created with manager: {employee.Manager.Name}");
+
+var clonedEmployee = (Employee)employee.Clone(true);
+
+Console.WriteLine($"employee Cloned with manager: {employee.Manager.Name}");
+
+manager.Name = "Changed name";
+Console.WriteLine("Original manager name changed");
+
+Console.WriteLine($"employee Cloned with manager: {clonedEmployee.Manager.Name}");
+
+Console.ReadLine();
