81273965adc92cff3ec6acff620512a5798d333f
Michal <michalbuczek00@gmail.com>
2024-01-18 10:50:11 +0100

    Prototype cleanup

---
+++ Creational/Prototype/Entities/Manager.cs
@@ -0,0 +1,17 @@
+﻿namespace Prototype.Entities
+{
+    public class Manager : Person
+    {
+        public override string Name { get; set; }
+
+        public Manager(string name)
+        {
+            this.Name = name;
+        }
+
+        public override Person Clone(bool deepClone = false)
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+}
